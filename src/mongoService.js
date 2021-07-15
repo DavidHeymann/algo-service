af93635215037ec7a8a422ce918d6d4b39292b10
DavidHeymann <heymann.david@gmai.com>
2021-07-15 18:51:35 +0300

    change port

--- src/mongoService.js
+++ src/mongoService.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'http://localhost:3002/api';
 
 const updateVideoDetails = async (videoId, updateDetails) => {
     try {
